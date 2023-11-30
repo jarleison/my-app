e583b3b1f26821c78d4d665b6aa39ec3b5b7fe2b
jarleison <jarleison.salas.2198@miremington.edu.co>
2023-11-30 00:40:28 -0500

    footer componente

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-info">
+          <h2>Contacto</h2>
+          <p>Dirección: Calle, Ciudad</p>
+          <p>Teléfono: (123) 456-7890</p>
+          <p>Email: info@pasteleriadelicia.com</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

7364cd21f98513342263fcc812d6c9f910f32a5d
jarleison <jarleison.salas.2198@miremington.edu.co>
2023-11-29 23:56:51 -0500

    crear bton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Boton from './cBoton/Boton';
 
 function App() {
+  const handleClick = () => {
+    alert('Haz hecho clic en el botón');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Mi Aplicación React</h1>
+        <Boton label="Haz clic" onClick={handleClick} />
+        {/* Otros componentes aquí */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +22,6 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          
         </a>
       </header>
     </div>
